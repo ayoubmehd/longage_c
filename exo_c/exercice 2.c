1b28eb74f51ad7ea35ed14bea49aa27f8d2d638a
ayoubmehd <ayoubelmahdaoui97@gmail.com>
2021-01-06 20:53:59 +0100

    fini le exarcece 2

--- exo_c/exercice 2.c
+++ exo_c/exercice 2.c
@@ -3,27 +3,49 @@
 
 int main() {
 	
-	int i, j;
+	int i, j, limit = 20;
 	
-	printf("   ");
+	printf("    ");
 	
-	for (i = 0; i <= 10; i++) {
-		printf("%d   ", i);	
+	for (i = 0; i <= limit; i++) {
+		
+		if (i - 1 < 10) {
+			printf("   ");
+		} else {
+			printf("  ");
+		}
+		
+		printf("%d", i);	
 	}
 	
 	printf("\n");
 	
 	
-	for (i = 0; i <= 10; i++) {
-		printf("%d   ", i);
-		for (j = 0; j <= 10; j++) {
-			if (i * j < 10) {
-				printf("%d   ", i * j);	
-			} else if (i * j >= 10) {
-				printf("%d  ", i * j);
-			} else if (i * j >= 100) {
-				printf("%d ", i * j);
+	for (i = 0; i <= limit; i++) {
+		
+		printf("%d", i);
+		if (i < 10) {
+			printf("  |");
+		} else {
+			printf(" |");
+		}
+		for (j = 0; j <= limit; j++) {
+				
+			int temp = i * j;
+			
+			int prevTemp = i * (j - 1); 
+					
+			
+			if (prevTemp < 10) {
+				printf("   ");	
+			} else if (prevTemp >= 10 && prevTemp < 100) {
+				printf("  ");
+			} else if (prevTemp >= 100) {
+				printf(" ");
 			}
+			//printf("(%d)", prevTemp);
+			printf("%d", temp);
+			
 		}
 		printf("\n");
 	
